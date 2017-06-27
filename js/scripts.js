dd73616120b7cd759906a5076e3d888e9384f43e
Camden Swita and Faunie Szeloczei <camden@>
2017-06-26 20:42:09 -0700

    adds additional contact fields in form and in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 //business logic
-function Contact(first, last) {
+function Contact(first, last, street, city, state, zip, country, phone, email) {
   this.firstName = first;
   this.lastName = last;
+  this.streetAddress = street;
+  this.cityAddress = city;
+  this.stateAddress = state;
+  this.zipAddress = zip;
+  this.countryAddress = country;
+  this.phone = phone;
+  this.email = email;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
 }
 
 // user interface logic
@@ -10,20 +21,49 @@ $(document).ready(function() {
     event.preventDefault();
 
     var inputtedFirstName = $("input#new-first-name").val();
+
     var inputtedLastName = $("input#new-last-name").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var inputtedStreetAddress = $("input#new-street-address").val();
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+    var inputtedCityAddress = $("input#new-city-address").val();
+
+    var inputtedStateAddress = $("input#new-state-address").val();
+
+    var inputtedZipAddress = $("input#new-zip-address").val();
+
+    var inputtedCountryAddress = $("input#new-country-address").val();
+
+    var inputtedPhone = $("input#new-phone").val();
+
+    var inputtedEmail = $("input#new-email").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedStreetAddress, inputtedCityAddress, inputtedStateAddress, inputtedZipAddress, inputtedCountryAddress, inputtedPhone, inputtedEmail);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName);
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
+      $("#show.first-name").text(newContact.firstName);
+      $("#show.last-name").text(newContact.lastName);
+      $("#show.street-address").text(newContact.streetAddress);
+      $("#show.city-address").text(newContact.cityAddress);
+      $("#show.state-address").text(newContact.stateAddress);
+      $("#show.zip-address").text(newContact.zipAddress);
+      $("#show.country-address").text(newContact.countryAddress);
+      $("#show.phone").text(newContact.phone);
+      $("#show.email").text(newContact.email);
     });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input#new-street-address").val("");
+    $("input#new-city-address").val("");
+    $("input#new-state-address").val("");
+    $("input#new-zip-address").val("");
+    $("input#new-country-address").val("");
+    $("input#new-phone").val("");
+    $("input#new-email").val("");
   });
 });
